ddafda931ef6113eb4e0d9240e479b65fa2ad6ad
Luciano Germani <lucianogermani93@gmail.com>
2025-05-19 13:10:49 +0200

    Feat: Create tailwindconfig for Theming

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      backgroundImage: {
+        "header": "url('/bg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
