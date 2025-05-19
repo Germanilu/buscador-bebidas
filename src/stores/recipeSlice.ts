16ae40410ab59767f09264672f0842cce81ecb9e
Luciano Germani <lucianogermani93@gmail.com>
2025-05-19 15:52:54 +0200

    Feat: add modal

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,13 +1,17 @@
 import type { StateCreator } from "zustand"
-import { getCategories, getRecipes } from "../services/RecipeService"
-import type { Categories, Drinks, SearchFilter } from '../types';
+import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService"
+import type { Categories, Drink, Drinks, Recipe, SearchFilter } from '../types';
 
 
 export type RecipesSliceType = {
     categories: Categories,
     drinks: Drinks,
+    selectedRecipe: Recipe,
+    modal:boolean,
     fetchCategories: () => Promise<void>
     searchRecipes: (SearchFilter: SearchFilter) => Promise<void>
+    selectRecipe : (id: Drink['idDrink']) => Promise<void>
+    closeModal: () => void
 }
 
 export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
@@ -17,6 +21,9 @@ export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
     drinks:{
         drinks: []
     },
+
+    selectedRecipe:{} as Recipe,
+    modal:false,
 
     fetchCategories: async() => {
       const categories = await getCategories()
@@ -29,6 +36,19 @@ export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
         const drinks = await getRecipes(filters)
         set({
             drinks
+        })
+    },
+    selectRecipe: async(id) => {
+        const selectedRecipe = await getRecipeById(id)
+        set({
+            selectedRecipe,
+            modal:true
+        })
+    },
+    closeModal: () => {
+        set({
+            modal: false,
+            selectedRecipe: {} as Recipe
         })
     }
 
