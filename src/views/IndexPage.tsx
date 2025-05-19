37e5e9edc88fec5da73c3d7cb0b597b3c786a770
Luciano Germani <lucianogermani93@gmail.com>
2025-05-19 13:26:06 +0200

    Feat: Setup multiples stores with zustand

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,5 +1,11 @@
+import { useAppStore } from "../stores/useAppStore"
+
+
+const categories = useAppStore((state) => state.categories);
+
 
 export default function IndexPage() {
+  
   return (
     <>
         <h1>Inicio</h1>
