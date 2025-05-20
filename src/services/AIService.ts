9caba6192bf4281eea590a7357581fddaa151e50
Luciano Germani <lucianogermani93@gmail.com>
2025-05-20 13:32:13 +0200

    Feat: Add AI component to generate response with AI meta

---
+++ src/services/AIService.ts
@@ -0,0 +1,16 @@
+
+import {streamText} from 'ai'
+import { openrouter } from '../lib/ai'
+
+export default {
+    async generateRecipe(prompt: string){
+        const result = streamText({
+            model:openrouter('meta-llama/llama-3.3-70b-instruct:free'),
+            prompt,
+            system:'Eres un bartender que tiene 40 años de experiencia ',
+            temperature:0
+        })
+
+        return result.textStream
+    }
+}
