bfab3e2495dda2a9a1dc799e4e2c0a5d9a5850ce
Luciano Germani <lucianogermani93@gmail.com>
2025-05-20 11:59:41 +0200

    Feat: Improoving performance with React router dom

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,8 @@
 import {lazy, Suspense} from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from './layouts/Layout'
-//
+
+//Improoving performance
 const IndexPage = lazy(() => import('./views/IndexPage'))
 const FavoritesPage = lazy(() => import('./views/FavoritesPage'))
 
